ff8bf24c4cf1067bf88f56d55c1c38472956657e
enyinnaya1234 <chemicalnilton@gmail.com>
2024-10-16 17:28:05 +0100

    Add project files.

---
+++ RedisDemo/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using RedisDemo.Models;
+using System.Diagnostics;
+
+namespace RedisDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IDistributedCache _distributedCache;
+        public HomeController(IDistributedCache distributedCache)
+        {
+                _distributedCache = distributedCache;
+        }
+        public async Task<ActionResult> SaveRedisCache()
+        {
+            var dashboardData = new DashboardData
+            {
+                TotalCustomerCount = 2100670,
+                TotalRevenue = 19980,
+                TopSellingCountryName = "Nigeria",
+                TopSellingProductName = "Ijebu Garri"
+            };
+            var tomorrow = DateTime.Now.Date.AddDays(1);
+            var totalSeconds = tomorrow.Subtract(DateTime.Now).TotalSeconds;
+            var distributedCacheEntryOptions = new DistributedCacheEntryOptions();
+            distributedCacheEntryOptions.AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(totalSeconds);
+            distributedCacheEntryOptions.SlidingExpiration = null;
+
+            var jsonData =  JsonConvert.SerializeObject(dashboardData);
+            await _distributedCache.SetStringAsync("DashboardData", jsonData, distributedCacheEntryOptions);
+             return View();
+        }
+    }
+}
---
+++ RedisDemo/Program.cs
@@ -0,0 +1,28 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetSection("RedisConnection").GetValue<string>("Configuration");
+    options.InstanceName = builder.Configuration.GetSection("RedisConnection").GetValue<string>("InstanceName");
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
